74e8a7b74b852571d155918762ba7aeb550bfed4
HtetAungHlaing-code <smileknight76@gmail.com>
2020-07-20 13:08:51 +0630

    Update

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:foodreviewapp/ui/view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            color: Colors.pinkAccent,
+          )),
+      home: RootPage(),
+    );
+  }
+}
